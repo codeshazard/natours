f4e1301dc269dcc6b78799b133ad1955cf0ce5c0
codeshazard <shaswatshrestha05@gmail.com>
2026-07-28 23:45:11 +0530

    Before map

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
@@ -11,10 +12,18 @@ const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
 //Globla Middlewares
+
+// Serving static files
+app.use(express.static(path.join(__dirname, 'starter/public')));
+
 //Set security HTTP headers
 app.use(helmet());
 
@@ -54,9 +63,6 @@ app.use(
   }),
 );
 
-//Serving static files
-app.use(express.static(`${__dirname}/starter/public`));
-
 //Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -73,6 +79,7 @@ app.post('/', (req, res) => {
 });*/
 
 //Routes
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
--- models/reviewModel.js
+++ models/reviewModel.js
@@ -33,6 +33,8 @@ const reviewSchema = new mongoose.Schema(
   },
 );
 
+reviewSchema.indexes({ tour: 1, user: 1 }, { unique: true });
+
 //Query Middleware
 reviewSchema.pre(/^find/, function () {
   //   this.populate({
